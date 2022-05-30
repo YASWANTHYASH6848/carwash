05ba84e78b0f44963df6fd2d3f48603cc06f27e6
YASWANTHYASH6848 <99253813+YASWANTHYASH6848@users.noreply.github.com>
2022-05-30 12:37:05 +0530

    Add files via upload

---
+++ Api-Gateway/src/main/java/com/gateway/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+
+}
---
+++ Api-Gateway/src/main/java/com/gateway/controller/FallbackController.java
@@ -0,0 +1,12 @@
+package com.gateway.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class FallbackController {
+
+	
+	
+}
